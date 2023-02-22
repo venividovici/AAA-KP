945d782e22998503fe5d8272bb60fdfaf233a369
fr4046ri-s <75071974+fr4046ri-s@users.noreply.github.com>
2023-02-22 11:53:07 +0100

    added promises

--- app.js
+++ app.js
@@ -47,7 +47,8 @@ app.get("/authenticate", function (req, res) {
 });
 
 //output page
-var dataInfo = "";
+var hsResponse = "";
+var fnResponse = ""; 
 
 app.get("/output", function (req, res) {
   const option1 = {
@@ -58,15 +59,6 @@ app.get("/output", function (req, res) {
       "Content-Type": "application/json",
     },
   };
-  request(option1, (error, response, body) => {
-    if (error) {
-      console.error(error);
-    } else {
-      //TODO handle hubspot information
-      console.log(body);
-      //dataInfo = JSON.stringify(body);
-    }
-  });
   const option2 = {
     method: "GET",
     url: "https://api.fortnox.se/3/companyinformation",
@@ -75,203 +67,268 @@ app.get("/output", function (req, res) {
     },
   };
 
-  request(option2, (error, response, body) => {
-    if (error) {
-      console.error(error);
-    } else {
-      //TODO handle fortnox information
-      console.log(body);
-      dataInfo += JSON.stringify(body);
-    }
+  // Use Promise.all to make both requests and wait for them to complete
+  Promise.all([requestPromise(option1), requestPromise(option2)])
+    .then((responses) => {
+      hsResponse = responses[0];
+      fnResponse = responses[1];
+
+      // TODO: handle hubspot and fortnox information
+
+      // Send the response to the client
+      res.render("pages/output", {
+        isLoading: false,
+        dataInfo: JSON.stringify(fnResponse + hsResponse)
+      })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).send("Error");
+        });
+    });
   });
 
-  res.render("pages/output", { dataInfo: dataInfo });
-});
-
-app.get("/openai", async function (req, res) {
-  const configuration = new Configuration({
-    apiKey: "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",
-  });
-
-  const openai = new OpenAIApi(configuration);
-  try {
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: "Write a haiku about insomnia.",
-    });
-
-    console.log(completion.data.choices[0].text);
-  } catch (error) {
-    console.log(error.response.status);
-    console.log(error.response.data);
-  }
-
-  res.redirect("/");
-});
-
-/* //404 page (HAS TO BE THE LAST @app.get ROUTE IN THIS DOCUMENT)
-app.get("/*", function (req, res) {
-  res.redirect("/");
-}); */
-
-var fnAuthCode;
-var fnAccessToken;
-
-//fortnox callback
-//(exchange code for token)
-app.get("/fn-callback", function (req, res) {
-  const { pathname, query } = url.parse(req.url);
-  const queryParams = querystring.parse(query);
-  if (queryParams.code) {
-    fnAuthCode = queryParams.code;
-
-    const options = {
-      method: "POST",
-      url: "https://apps.fortnox.se/oauth-v1/token",
-      headers: {
-        "Content-Type": "application/x-www-urlencoded",
-        Authorization: "Basic MjJGcDM1TmlCR1VEOnBtUkhkSmRsY2w=",
-      },
-      form: {
-        grant_type: "authorization_code",
-        code: fnAuthCode,
-        redirect_uri: "http://localhost:3000/fn-callback",
-      },
-    };
-    request(options, (error, response, body) => {
-      if (error) {
-        console.error(error);
-      } else {
-        console.log(body);
-        const responseBody = JSON.parse(body);
-        fnAccessToken = responseBody.access_token;
-        /* var fortnoxButton = document.getElementById('fortnoxButton'); //Funkar detta??
-                                fortnoxButton.disabled = true;*/
-      }
-    });
-  }
-  res.redirect("/authenticate");
-});
-
-//fortnox OAuth
-app.get("/fn-oauth", function (req, res) {
-  //return to landing page after auth
-  res.redirect(
-    "https://apps.fortnox.se/oauth-v1/auth?client_id=22Fp35NiBGUD&redirect_uri=http://localhost:3000/fn-callback&scope=companyinformation&state=somestate&access_type=offline&response_type=code&account_type=service"
-  );
-});
-
-var hsAuthCode;
-var hsAccessToken;
-
-//hubspot callback
-//(exchange code for token)
-app.get("/hs-callback", function (req, res) {
-  const { pathname, query } = url.parse(req.url);
-  const queryParams = querystring.parse(query);
-  if (queryParams.code) {
-    hsAuthCode = queryParams.code;
-
-    const options = {
-      method: "POST",
-      url: "https://api.hubapi.com/oauth/v1/token",
-      headers: {
-        "Content-Type": "application/x-www-urlencoded",
-      },
-      form: {
-        grant_type: "authorization_code",
-        client_id: "afd563db-c00e-4d47-b52d-d421800d6c01",
-        client_secret: "998b8f49-1167-4125-8961-85452c927589",
-        code: hsAuthCode,
-        redirect_uri: "http://localhost:3000/hs-callback",
-      },
-    };
-    request(options, (error, response, body) => {
-      if (error) {
-        console.error(error);
-      } else {
-        console.log(body);
-        const responseBody = JSON.parse(body);
-        hsAccessToken = responseBody.access_token;
-      }
-    });
-  }
-  res.redirect("/authenticate");
-});
-
-//hubspot OAuth
-app.get("/hs-oauth", function (req, res) {
-  //build the authurl
-  const authUrl =
-    "https://app.hubspot.com/oauth/authorize" +
-    `?client_id=afd563db-c00e-4d47-b52d-d421800d6c01` +
-    `&scope=crm.objects.contacts.read` +
-    `&redirect_uri=http://localhost:3000/hs-callback`;
-  +`&state=hubspot`;
-
-  // Redirect the user
-  res.redirect(authUrl);
-});
-
-/*
-app.get('/fn-info', function (req, res) {
-        const options = {
-                method: 'GET',
-                url: 'https://api.fortnox.se/3/companyinformation',
-                headers: {
-                        'Authorization': "Bearer " + fnAccessToken
-                }
+  // Wrap the request function in a promise for easier use with Promise.all
+  function requestPromise(options) {
+    return new Promise((resolve, reject) => {
+      request(options, (error, response, body) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(body);
         }
+      });
+    });
+  }
 
-        request(options, (error, response, body) => {
-                if (error) {
-                        console.error(error)
-                } else {
-                        //TODO handle fortnox information
-                        console.log(body);
-                }
-        })
-        res.redirect('/');
-});*/
 
-//info from hubspot and fortnox
-/* app.get("/info", function (req, res) {
-  const option1 = {
-    method: "GET",
-    url: "https://api.hubspot.com/crm/v3/objects/contacts",
-    headers: {
-      Authorization: "Bearer " + hsAccessToken,
-      "Content-Type": "application/json",
-    },
-  };
-  request(option1, (error, response, body) => {
-    if (error) {
-      console.error(error);
-    } else {
-      //TODO handle hubspot information
-      console.log(body);
+  /*app.get("/output", function (req, res) {
+    const option1 = {
+      method: "GET",
+      url: "https://api.hubspot.com/crm/v3/objects/contacts",
+      headers: {
+        Authorization: "Bearer " + hsAccessToken,
+        "Content-Type": "application/json",
+      },
+    };
+    request(option1, (error, response, body) => {
+      if (error) {
+        console.error(error);
+      } else {
+        //TODO handle hubspot information
+        console.log(body);
+        dataInfo = JSON.stringify(body);
+        isLoading=false; 
+        res.render("pages/output", { dataInfo: dataInfo, isLoading: isLoading });
+      
+      }
+    });
+    const option2 = {
+      method: "GET",
+      url: "https://api.fortnox.se/3/companyinformation",
+      headers: {
+        Authorization: "Bearer " + fnAccessToken,
+      },
+    };
+  
+    request(option2, (error, response, body) => {
+      if (error) {
+        console.error(error);
+      } else {
+        //TODO handle fortnox information
+        console.log(body);
+        dataInfo += JSON.stringify(body);
+        isLoading=false; 
+        res.render("pages/output", { dataInfo: dataInfo, isLoading: isLoading });
+      }
+    });
+  
+   // res.render("pages/output", { dataInfo: dataInfo, isLoading: isLoading });
+  }); */
+
+  app.get("/openai", async function (req, res) {
+    const configuration = new Configuration({
+      apiKey: "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",
+    });
+
+    const openai = new OpenAIApi(configuration);
+    try {
+      const completion = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: "Write a haiku about insomnia.",
+      });
+
+      console.log(completion.data.choices[0].text);
+    } catch (error) {
+      console.log(error.response.status);
+      console.log(error.response.data);
     }
-  });
-  const option2 = {
-    method: "GET",
-    url: "https://api.fortnox.se/3/companyinformation",
-    headers: {
-      Authorization: "Bearer " + fnAccessToken,
-    },
-  };
 
-  request(option2, (error, response, body) => {
-    if (error) {
-      console.error(error);
-    } else {
-      //TODO handle fortnox information
-      console.log(body);
+    res.redirect("/");
+  });
+
+  /* //404 page (HAS TO BE THE LAST @app.get ROUTE IN THIS DOCUMENT)
+  app.get("/*", function (req, res) {
+    res.redirect("/");
+  }); */
+
+  var fnAuthCode;
+  var fnAccessToken;
+
+  //fortnox callback
+  //(exchange code for token)
+  app.get("/fn-callback", function (req, res) {
+    const { pathname, query } = url.parse(req.url);
+    const queryParams = querystring.parse(query);
+    if (queryParams.code) {
+      fnAuthCode = queryParams.code;
+
+      const options = {
+        method: "POST",
+        url: "https://apps.fortnox.se/oauth-v1/token",
+        headers: {
+          "Content-Type": "application/x-www-urlencoded",
+          Authorization: "Basic MjJGcDM1TmlCR1VEOnBtUkhkSmRsY2w=",
+        },
+        form: {
+          grant_type: "authorization_code",
+          code: fnAuthCode,
+          redirect_uri: "http://localhost:3000/fn-callback",
+        },
+      };
+      request(options, (error, response, body) => {
+        if (error) {
+          console.error(error);
+        } else {
+          console.log(body);
+          const responseBody = JSON.parse(body);
+          fnAccessToken = responseBody.access_token;
+          /* var fortnoxButton = document.getElementById('fortnoxButton'); //Funkar detta??
+                                  fortnoxButton.disabled = true;*/
+        }
+      });
     }
+    res.redirect("/authenticate");
   });
-  res.redirect("/output");
-}); */
 
-// Start server on port 3000
-app.listen(3000, function (req, res) {
-  console.log("Server started at http://localhost:3000");
-});
+  //fortnox OAuth
+  app.get("/fn-oauth", function (req, res) {
+    //return to landing page after auth
+    res.redirect(
+      "https://apps.fortnox.se/oauth-v1/auth?client_id=22Fp35NiBGUD&redirect_uri=http://localhost:3000/fn-callback&scope=companyinformation&state=somestate&access_type=offline&response_type=code&account_type=service"
+    );
+  });
+
+  var hsAuthCode;
+  var hsAccessToken;
+
+  //hubspot callback
+  //(exchange code for token)
+  app.get("/hs-callback", function (req, res) {
+    const { pathname, query } = url.parse(req.url);
+    const queryParams = querystring.parse(query);
+    if (queryParams.code) {
+      hsAuthCode = queryParams.code;
+
+      const options = {
+        method: "POST",
+        url: "https://api.hubapi.com/oauth/v1/token",
+        headers: {
+          "Content-Type": "application/x-www-urlencoded",
+        },
+        form: {
+          grant_type: "authorization_code",
+          client_id: "afd563db-c00e-4d47-b52d-d421800d6c01",
+          client_secret: "998b8f49-1167-4125-8961-85452c927589",
+          code: hsAuthCode,
+          redirect_uri: "http://localhost:3000/hs-callback",
+        },
+      };
+      request(options, (error, response, body) => {
+        if (error) {
+          console.error(error);
+        } else {
+          console.log(body);
+          const responseBody = JSON.parse(body);
+          hsAccessToken = responseBody.access_token;
+        }
+      });
+    }
+    res.redirect("/authenticate");
+  });
+
+  //hubspot OAuth
+  app.get("/hs-oauth", function (req, res) {
+    //build the authurl
+    const authUrl =
+      "https://app.hubspot.com/oauth/authorize" +
+      `?client_id=afd563db-c00e-4d47-b52d-d421800d6c01` +
+      `&scope=crm.objects.contacts.read` +
+      `&redirect_uri=http://localhost:3000/hs-callback`;
+    +`&state=hubspot`;
+
+    // Redirect the user
+    res.redirect(authUrl);
+  });
+
+  /*
+  app.get('/fn-info', function (req, res) {
+          const options = {
+                  method: 'GET',
+                  url: 'https://api.fortnox.se/3/companyinformation',
+                  headers: {
+                          'Authorization': "Bearer " + fnAccessToken
+                  }
+          }
+  
+          request(options, (error, response, body) => {
+                  if (error) {
+                          console.error(error)
+                  } else {
+                          //TODO handle fortnox information
+                          console.log(body);
+                  }
+          })
+          res.redirect('/');
+  });*/
+
+  //info from hubspot and fortnox
+  /* app.get("/info", function (req, res) {
+    const option1 = {
+      method: "GET",
+      url: "https://api.hubspot.com/crm/v3/objects/contacts",
+      headers: {
+        Authorization: "Bearer " + hsAccessToken,
+        "Content-Type": "application/json",
+      },
+    };
+    request(option1, (error, response, body) => {
+      if (error) {
+        console.error(error);
+      } else {
+        //TODO handle hubspot information
+        console.log(body);
+      }
+    });
+    const option2 = {
+      method: "GET",
+      url: "https://api.fortnox.se/3/companyinformation",
+      headers: {
+        Authorization: "Bearer " + fnAccessToken,
+      },
+    };
+  
+    request(option2, (error, response, body) => {
+      if (error) {
+        console.error(error);
+      } else {
+        //TODO handle fortnox information
+        console.log(body);
+      }
+    });
+    res.redirect("/output");
+  }); */
+
+  // Start server on port 3000
+  app.listen(3000, function (req, res) {
+    console.log("Server started at http://localhost:3000");
+  });
