6e4092ed9466f2e23fbac1b7fe00ff2b5697f22e
TorstenTyp <torsten.stromberg@yahoo.se>
2023-02-23 13:11:53 +0100

    Download

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,3 +2,12 @@ function onClickToOutput() {
   document.getElementById("loadingText").innerHTML = "Laddar…";
   location.href = "/loading";
 }
+function downloadFile(){
+  var content = 'fnResponse'; 
+  var blob = new Blob([content], {type: "text/csv"});
+  var fileLink = document.createElement('a');
+  fileLink.href = window.URL.createObjectURL(blob);
+  const date = new Date(); 
+  fileLink.download = 'AAA ' + date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+  fileLink.click();
+ }
