a2582ccf235b2092d36aeb3717fb20cd810ad8e7
venividovici <93551061+venividovici@users.noreply.github.com>
2023-03-08 14:34:23 +0100

    new prompt

--- ai.js
+++ ai.js
@@ -5,13 +5,13 @@ async function requestOpenAI(jsonData, chunkSize) {
     apiKey: process.env.OPENAI_API_KEY,
   });
   let n = 0;
-  
-  var iterableData = JSON.stringify(jsonData);
-  var dataChunks = []
-  
-  for(var x = 0; x<iterableData.length;x+=chunkSize){
-    dataChunks[n++] = iterableData.substring(x,x+chunkSize);
-    console.log(`\n\n#${n}:${dataChunks[n-1]}`);
+
+  var dataString = JSON.stringify(jsonData);
+  var dataChunks = [];
+
+  for (let x = 0; x < dataString.length; x += chunkSize) {
+    dataChunks[n++] = dataString.substring(x, x + chunkSize);
+    console.log(`\n\n#${n}:${dataChunks[n - 1]}`);
   }
 
   const openai = new OpenAIApi(configuration);
@@ -19,40 +19,53 @@ async function requestOpenAI(jsonData, chunkSize) {
     var summarizedData = "";
     console.log("\n\n\n");
 
-    var entryCount = dataChunks.length;
+    let chunkCount = dataChunks.length;
 
-    for (var i = 0; i < entryCount; i++) {
+    let preprocessing_prompt = `We are preprocessing large amounts of data using GPT-3 for 
+	the purpose of fitting it into a final <4000 token GPT-3 prompt that will
+	generate some interesting insights about the summarized data.
+
+	The data comes from a business development consulting firm, and contains 
+	information about invoices (from their ERP system), and clients (from 
+	their CRM system).
+	
+	the generated content from the final GPT-3 prompt will be used for writing a
+	powerBI report.
+	
+	The data needs to be preprocessed so that it can be used by the final GPT-3 
+	generation to create an accurate and coherent analysis.
+	
+	To get around the 4000-token limit that prevents us from prompting the entire
+	dataset at once, we will iterate through chunks of the dataset at a time.
+
+	We will receive small portions at a time of a large, unsorted, merged dataset 
+	containing both the invoice data and the client data.
+
+	For each step, we receive both the preprocessed summarized data, and the
+	unsummarized data that we need to preprocess in the same way.
+
+	We look at that unsummarized data chunk and determine whether its information and
+	trends align with the summarization of previous data chunks; if it does not, we 
+	need to consider how this new data chunks changes the summarization of previous data chunks.
+	Then we simply write out the data preprocessing result that combines old (and possibly amended)
+	preprocessed data with the newly preprocessed data chunk.
+
+	Previously processed data:
+	unprocessed data chunk:
+	Data preprocessing result:
+
+	Previously processed data:${summarizedData}
+	unprocessed data chunk: ${dataChunks[i]}
+	Data preprocessing result:`;
+
+    let analysis_prompt = "";
+
+    for (let i = 0; i < chunkCount; i++) {
       console.log(`batch ${i + 1} is being processed...\n`);
       const completion = await openai.createCompletion({
         model: "text-davinci-003",
         max_tokens: 1500,
-        prompt: `You are going to receive some summarized data and a
-                chunk of unsummarized data. Summarize/condense the databatch and 
-                integrate it into the summarized data provided. The summary should
-                be more or less a single sentence per databatch - if a pattern
-                repeats itself over databatches, then they can be summarized together
-                in a single sentence. If no summarized data is provided, then simply 
-                summarize the databatch for itself.
-                
-                Do not prune data, and do not add data that does not exist! 
-                The data in the summarized data section is to be preserved, if you 
-                do decide to prune any data you must provide your reason for doing 
-                so, on top of the summarized data.
-
-                the purpose is to create a complete overview of a large quantity 
-                of data. Your task is to summarize the provided databatch, append 
-                and integrate it into the summarized data provided. If the Databatch 
-                provided below is numbered ${entryCount} of ${entryCount}, it is the 
-                final one to be sent to you and you should, at the end of the summary, 
-                provide some further insights and analysis on you summarized data 
-                beyond just the condensed summary. Don't make any assumptions about the
-                data or its context, beyond what you can know for sure! identify patterns
-                and correlations.
-                
-                \n\nSummarized Data:${summarizedData}\n\n
-                Databatch( no. ${i + 1} of ${entryCount}):${dataChunks[i].toString()}
-                
-                \n\nNew version of Summarized Data that includes summarization of databatch ${i+1}:\n`,
+        prompt: i == chunkCount - 1 ? analysis_prompt : preprocessing_prompt,
       });
       summarizedData = completion.data.choices[0].text;
     }
