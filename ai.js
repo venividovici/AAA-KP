18c132cfe2a5e13e840021671d6fd5182e7be5e4
venividovici <93551061+venividovici@users.noreply.github.com>
2023-03-10 02:55:37 +0100

    Update ai.js

--- ai.js
+++ ai.js
@@ -1,5 +1,12 @@
 const { Configuration, OpenAIApi } = require("openai");
 
+
+/**
+ * 
+ * @param {*} jsonData json data to be stringified, split and preprocessed
+ * @param {*} chunkSize the number of individual characters per chunk
+ * @returns 
+ */
 async function requestOpenAI(jsonData, chunkSize) {
   const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
@@ -17,48 +24,68 @@ async function requestOpenAI(jsonData, chunkSize) {
   const openai = new OpenAIApi(configuration);
   try {
     var summarizedData = "";
+    var temp;
     console.log("\n\n\n");
 
     let chunkCount = dataChunks.length;
 
-    let preprocessing_prompt = `We are preprocessing large amounts of data using GPT-3 for 
-	the purpose of fitting it into a final <4000 token GPT-3 prompt that will
-	generate some interesting insights about the summarized data.
+    let preprocessing_prompt = `
+    We are using GPT-3 to analyze large amounts of data from a business development 
+    consulting firm. The data includes information about invoices (from their ERP 
+    system) and clients (from their CRM system). Our goal is to generate 
+    interesting insights about the data in a final GPT-3 prompt, which will be used 
+    for writing a Power BI report.
 
-	The data comes from a business development consulting firm, and contains 
-	information about invoices (from their ERP system), and clients (from 
-	their CRM system).
-	
-	the generated content from the final GPT-3 prompt will be used for writing a
-	powerBI report.
-	
-	The data needs to be preprocessed so that it can be used by the final GPT-3 
-	generation to create an accurate and coherent analysis.
-	
-	To get around the 4000-token limit that prevents us from prompting the entire
-	dataset at once, we will iterate through chunks of the dataset at a time.
+    However, due to the 4000-token limit of GPT-3, we need to preprocess the data in 
+    chunks to make it more manageable. To do this, we will iterate through the 
+    dataset in smaller portions and preprocess each chunk before feeding it into the 
+    final prompt.
+    
+    For each iteration, we will receive an unsorted, merged dataset that contains 
+    both invoice data and client data. Our first step will be to preprocess the data 
+    by summarizing it into its essential components, while retaining as much 
+    meaningful information as possible.
+    
+    To achieve this, we will use clustering and dimensionality reduction techniques 
+    to group similar data points together and eliminate redundant information. We will 
+    also look for trends and patterns in the data to identify key insights that can 
+    be used in the final prompt.
+    
+    Once we have preprocessed the data, we will compare it to previous iterations to 
+    ensure consistency and identify any changes in trends or patterns. If necessary, 
+    we will adjust our preprocessing methods to account for these changes.
+    
+    Finally, we will feed the preprocessed data into the final GPT-3 prompt to 
+    generate interesting insights and analysis. The output will be used to create a 
+    Power BI report that presents the findings in a clear and concise manner.
+    
+    Previous processed data:
+    ${summarizedData}
+    
+    Unprocessed data chunk:
+    ${dataChunks[i]}
+    
+    Data preprocessing result:`;
 
-	We will receive small portions at a time of a large, unsorted, merged dataset 
-	containing both the invoice data and the client data.
+    let analysis_prompt = `
+    Given the preprocessed data, generate insights about the business development 
+    consulting firm's clients and invoices:
 
-	For each step, we receive both the preprocessed summarized data, and the
-	unsummarized data that we need to preprocess in the same way.
+    Identify the top clients by revenue and provide insights into their spending 
+    patterns.
+    Determine the most common invoice categories and their associated revenue.
+    Identify any trends or patterns in the invoice data, such as fluctuations in 
+    revenue or an increase in certain types of invoices.
+    Provide recommendations for increasing revenue, reducing costs, or improving 
+    client retention based on the analysis of the data.
 
-	We look at that unsummarized data chunk and determine whether its information and
-	trends align with the summarization of previous data chunks; if it does not, we 
-	need to consider how this new data chunks changes the summarization of previous data chunks.
-	Then we simply write out the data preprocessing result that combines old (and possibly amended)
-	preprocessed data with the newly preprocessed data chunk.
+    Please provide detailed insights for each of the above questions, supported by 
+    relevant data and examples. Keep in mind that the generated insights should be 
+    accurate, coherent, and valuable for the business development consulting firm's 
+    future decisions.
 
-	Previously preprocessed data:
-	unprocessed data chunk:
-	Data preprocessing result:
-
-	Previously preprocessed data:${summarizedData}
-	unprocessed data chunk: ${dataChunks[i]}
-	Data preprocessing result:`;
-
-    let analysis_prompt = "";
+    Preprocessed data: ${summarizedData}
+    Business Intelligence Report:`;
 
     for (let i = 0; i < chunkCount; i++) {
       console.log(`batch ${i + 1} is being processed...\n`);
@@ -67,7 +94,8 @@ async function requestOpenAI(jsonData, chunkSize) {
         max_tokens: 1500,
         prompt: i == chunkCount - 1 ? analysis_prompt : preprocessing_prompt,
       });
-      summarizedData = completion.data.choices[0].text;
+      summarizedData = temp + completion.data.choices[0].text;
+      temp = completion.data.choices[0].text;
     }
 
     console.log(summarizedData);
