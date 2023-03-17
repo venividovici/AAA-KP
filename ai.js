7ad56b34f527a13b2b4aacccca9787d73dd1fbb6
TorstenTyp <torsten.stromberg@yahoo.se>
2023-03-17 09:45:14 +0100

    Listprompt

--- ai.js
+++ ai.js
@@ -14,7 +14,7 @@ async function requestOpenAI(jsonData, chunkSize) {
 
   var dataString = jsonData;
   var dataChunks = [];
-  var count = 0; 
+  var count = 0;
 
   for (var x = 0; x < dataString.length; x += chunkSize) {
     dataChunks[n++] = dataString.substring(x, x + chunkSize);
@@ -24,70 +24,70 @@ async function requestOpenAI(jsonData, chunkSize) {
   const openai = new OpenAIApi(configuration);
   try {
     var summarizedData = "";
-    var temp;
+    var temp = '';
     console.log("\n\n\n");
 
     var chunkCount = dataChunks.length;
 
-    var preprocessing_prompt = `
-    Vi använder GPT-3 för att analysera stora mängder data från ett 
-     konsultföretag. Uppgifterna innehåller information om fakturor (från deras bokföringssystem)
-      och klienter (från deras CRM-system). Vårt mål är att generera
-     intressanta insikter om data i en sista GPT-3-prompt, som kommer att användas
-     för att skriva en Power BI-rapport.
+    function preprocessing_prompt(count){
+      return  `
+      Output är en lista av alla värden där det står "CustomerName": eller "name": innan ifrån JSON-data.
+      Output är en lista över CustomerName eller name från JSON-data + tidigare lista. 
+      Output är alltså sammansatt lista som är längre än tidigare lista. 
+  
+      JSON-data:
+      #18:tps://api.fortnox.se/3/invoices/1692","Balance":0,"Booked":true,"Cancelled":false,"CostCenter":"","Currency":"SEK",
+      "CurrencyRate":"1","CurrencyUnit":1,"CustomerName":"MalmbergGruppen Aktiebolag","CustomerNumber":"324","DocumentNumber":"1692",
+      "DueDate":"2019-12-31","ExternalInvoiceReference1":"","ExternalInvoiceReference2":"","InvoiceDate":"2019-12-01","InvoiceType":"INVOICE",
+      "NoxFinans":false,"OCR":"169268","VoucherNumber":30,"VoucherSeries":"B","VoucherYear":2,"WayOfDelivery":"","TermsOfPayment":"30",
+      "Project":"","Sent":true,"Total":258500,"FinalPayDate":"2020-01-22"},{"@url":"https://api.fortnox.se/3/invoices/1607","Balance":0,"Booked":true,
+      "Cancelled":false,"CostCenter":"","Currency":"SEK","CurrencyRate":"1","CurrencyUnit":1,"CustomerName":"HH Ferries AB PSZ3023",
+      "CustomerNumber":"290","DocumentNumber":"1607","DueDate":"2018-12-01","ExternalInvoiceReference1":"","ExternalInvoiceReference2":"","InvoiceDate":"2018-11-01","InvoiceType":"INVOICE",
+      "NoxFinans":false,"OCR":"160762","Vouche
+      
+       Tidigare lista:
+       IKEA Ämhult AB
+       Volvo AB
+      
+       Output:
+       IKEA Ämhult AB
+       Volvo AB
+       MalmbergGruppen Aktiebolag
+       HH Ferries AB PSZ3023
+  
+      Output är en lista av alla värden där det står "CustomerName": eller "name": innan ifrån JSON-data.
+      Output är en lista över CustomerName eller name från JSON-data + tidigare lista. 
+      Output är alltså sammansatt lista som är längre än tidigare lista. 
+  
+      JSON-data:
+      ${dataChunks[count]}
+      
+       Tidigare lista:
+       ${summarizedData}
+      
+       Output:`;
+    } 
 
-     Men på grund av gränsen på 4000 token för GPT-3 måste vi förbehandla data i
-     bitar för att göra det mer hanterbart. För att göra detta kommer vi att iterera igenom
-     dataset i mindre portioner, matar in dem i GPT-3 som kommer att förbehandla var och en
-     bit innan den matas in i den sista prompten som indata.
-    
-     För varje iteration kommer vi att få en osorterad, sammanslagen datauppsättning som innehåller
-     både fakturadata och kunddata. Vårt första steg blir att förbehandla data
-     använder GPT-3 genom att sammanfatta den i dess väsentliga komponenter, samtidigt som den behålls som
-     så mycket meningsfull information som möjligt. Vi kommer också att leta efter trender och mönster i data för att identifiera viktiga insikter som kan
-     användas i den sista prompten.
-    
-     När vi har förbehandlat datan kommer vi att jämföra det med tidigare iterationer till
-     säkerställa konsekvens och identifiera eventuella förändringar i trender eller mönster.
-    
-     Slutligen kommer vi att mata in den förbehandlade informationen i den slutliga GPT-3-prompten till
-     generera intressanta insikter och analyser. Utdata kommer att användas för att skapa en
-     Power BI-rapport som presenterar resultaten på ett tydligt och kortfattat sätt. Det är viktigt att alla relevanta variabler
-     och intressanta datapunkter är med.
-    
-     Tidigare bearbetade data:
-     ${summarizedData}
-    
-     Obearbetad data:
-     ${dataChunks[count]}
-    
-     Resultat från förbearbetning av data:`;
     var analysis_prompt = `
-    Givet den förbearbetade datan, svara med konkreta tips på hur man kan använda denna data för att skapa
-    en bra Power-BI rapport, gärna stegvis. Svaret bör vara omkring 150 ord långt. 
+    Nedan kommer en lista på olika företag som är kunder, listan innehåller också information om fakturor från dem.
 
-     Förslag på insikter som kan vara bra att få med i sin rapport skulle kunna vara till exempel ett
-     cirkeldiagram över de bästa kunderna efter intäkter. Ett annat exempel är att identifiera eventuella trender
-     eller mönster i fakturadata, till exempel fluktuationer i intäkter eller en ökning av vissa typer av fakturor.
+     Lista: ${summarizedData}
      
-     Vänligen ge detaljerade insikter för var och en av ovanstående frågor, med stöd av
-     relevanta data och exempel. Tänk på att de genererade insikterna bör vara det
-     korrekt, sammanhängande och värdefullt för konsultföretaget för affärsutveckling
-     framtida beslut. Föreslå ingenting som inte framgår i datan, det vill säga, hitta inte på någonting nytt.
-
-     Vänligen ge några exempel på företag och dess fakturor. Företag hittar du efter "CustomerName": och för att hitta vad en faktura tillhör för företag
-     leta efter "name": 
-
-     Förbearbetad data: ${summarizedData}
-     Tips för skapande av en Business Intelligence-rapport:`;
+     Jag vill skapa en PowerBI-rapport av data som innehåller denna information, ge mig konkreta tips på hur jag gör det på bästa sätt.
+     Ta intressanta rader från listan och använd som exempel i ditt svar. Svara inte med listan eller någon ny lista, utan bara med tips och råd.
+     Använd gärna exempel direkt från listan.
+     Svaret ska vara ett tydligt och konkret förslag på hur jag ska använda just den här informationen i en PowerBI-rapport.
+     
+     Tips på hur man skapar en bra PowerBI-rapport utifrån informationen:`;
 
     for (var i = 0; i < chunkCount; i++) {
-      console.log(`batch ${i + 1} is being processed...\n`);
-      count++; 
+      // console.log(`batch ${i + 1} is being processed...\n`);
+      count++;
+      console.log(summarizedData);
       const completion = await openai.createCompletion({
         model: "text-davinci-003",
         max_tokens: 1500,
-        prompt: i == chunkCount - 1 ? analysis_prompt : preprocessing_prompt,
+        prompt: i == chunkCount - 1 ? analysis_prompt : preprocessing_prompt(count),
       });
       summarizedData = temp + completion.data.choices[0].text;
       temp = completion.data.choices[0].text;
@@ -101,3 +101,21 @@ async function requestOpenAI(jsonData, chunkSize) {
 }
 
 module.exports = requestOpenAI;
+
+
+// Här kommer en mängd data, datan är del av en större datamängd men på grund av token-gränsen på
+//     GPT-3 så kommer bara en del av datan i taget. Datan avser information om kunder som ett företag har
+//     och information om fakturor. All information kommer ifrån och avser samma företag i Lund. 
+//     Datan innehåller 1) information om kunderna (företagen) 2) Information om Deals och 3) Information om fakturor.
+//     Svara med en ny lista. Om det finns något i datan som troligtvis är ett namn på ett företag, lägg till det i den nya listan.
+//     Innan företagsnamnen brukar det står "name" eller "CustomerName", så leta efter det.
+//     Den nya listan ska börja med den tidigare listan, och sen namnen på företagen.  
+
+//     Svara med en lista över de relevanta raderna, lägg inte till något om det redan står på listan. Nedan är en tidigare lista som redan har
+//     sammanställts, listan du svarar med ska vara en förlängning av den listan. Det vill säga: Skapa en egen lista och sätt ihop
+//     den längst bak på den tidigare listan och svara sedan med den nya längre listan. Lägg inte till något i listan som inte redan 
+//     finns i datan (under "Data att göra den nya listan på:").
+//     Om det inte finns något relevant i datan så är det bäst att inte lägga till någonting alls utan bara svara med den tidigare listan.
+//     Om det står "undefined" under "Tidigare lista:" så är detta den första gången datan summeras till en lista, strunta då i den tidigare listan och
+//     svara endast med den nya listan. Listan kommer alltså bara vara namn på företag, först de som finns (om några) i den tidigare listan
+//     och sen de som hittats (om några) ifrån datan.
