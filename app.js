d042db63fb857c0212cbeb1a599ab49e66fddfea
TorstenTyp <torsten.stromberg@yahoo.se>
2023-03-17 15:08:29 +0100

    hej

--- app.js
+++ app.js
@@ -126,6 +126,8 @@ app.get("/generate", function (req, res) {
         res.status(500).send("Error");
       });
   }
+          res.redirect("/output");
+
 });
 
 //Fortnox callback (exchange code for token)
